9136cd13defff7ac58e19a3ee31c5ffca2797249
HorridModz <105762560+HorridModz@users.noreply.github.com>
2022-11-03 18:35:54 -0400

    Hello Version Control! IDK what I'm doing or how version control or git works but maybe in 5 years I'll be famous and people will be reading this.

--- Python/DeobfuscationRefactored.py
+++ Python/DeobfuscationRefactored.py
@@ -3,25 +3,69 @@ import string
 import copy
 
 def removesubstring(s: str,sub: str) -> str:
+    #Done
+    """
+    Remove one substring from a string
+
+    Example:
+        String: "Removing Substrings"
+        Sub:    "ing"
+        Return: "Remov Substrs"
+
+    Arguments:
+        s: string to remove substring from
+        sub: substring to remove from string
+    Return:
+        string with substring removed
+    """
     return(s.replace(sub,""))
 
 def removesubstrings(s: str,subs: list[str]) -> str:
+    #Done
+    """
+    Remove multiple substring from a string, in order of list
+    Example:
+        String: "Removing Substrings"
+        Subs:    ["e","in","ing"]
+        Return: "Rmovg Substrgs"
+    Arguments:
+        s: string to remove substrings from
+        subs: list of substrings to remove from string
+    Return:
+        string with substrings removed
+    """
     for sub in subs:
         s = removesubstring(s,sub)
     return(s)
 
 def removeallwhitespace(s: str) -> str:
-    """Removes all whitespaces from a string, doesn't just trim leading and trailing"""
+    #Done
+    """
+    Removes all whitespace from a string
+    Does not just trim leading and trailing. For that, use the trim function.
+     Example:
+        String: "   Whitespace  will be      removed from
+        this string"
+        Return: "Whitespacewillberemovedfromthisstring"
+
+    Arguments:
+        s: string to remove substrings from
+        subs: list of substrings to remove from string
+    Return:
+        string with substring removes
+    """
     _whitespace = [*string.whitespace]
     return(removesubstrings(s,_whitespace))
 
 def iswhitespace(s: str) -> str:
+    #Done, but maybe could be optimized
     for thisletter in s:
         if not(thisletter.isspace()):
             return(False)
     return(True)
 
 def trim(s: str,leading=True, trailing=True) -> str:
+    #Done
     if leading and trailing:
         return s.strip()
     elif leading:
@@ -32,6 +76,7 @@ def trim(s: str,leading=True, trailing=True) -> str:
         return s
 
 def getlines(s: str,toremoveblanklines=False,totrimlines=False) -> list[str]:
+    # Done, but maybe could be optimized
     """
     1. Creating a new list is inefficient, modifying existing list would be ideal
     2. Directly using s.splitlines() instead of using lines variable may be faster, but sacrifices readability
@@ -46,19 +91,25 @@ def getlines(s: str,toremoveblanklines=False,totrimlines=False) -> list[str]:
     return(newlines)
 
 def dumpcs_isvalid(dumpcs: str) -> bool:
+    #Working but not done
     """
     Bad detection, needs proper algorithm
     """
     return "// Image" in dumpcs and "// RVA: 0x" in dumpcs and "// Namespace:" in dumpcs and " TypeDefIndex: " in dumpcs
 
 def dumpcs_checkformat(dumpcs: str) -> list[str]:
+    #Not done
     """
-    Check for errors in dump.cs format
+    Scan dump.cs for unexpected formatting
     Returns list of unexpected formats
     """
-
-def dumpcs_hasattributes(dumpcs: str):
     return None
+
+def dumpcs_hasattributes(dumpcs: str) -> bool:
+    """
+    Bad detection, needs proper algorithm
+    """
+    return "[CompilerGeneratedAttribute]" in dumpcs
 
 
 def dumpcs_constructor(path: str,attributeswarning=False) -> str:
